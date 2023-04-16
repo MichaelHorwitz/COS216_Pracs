70a1387cfaf9b69587a58f7eb261e0e0892d8789
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-04-17 00:12:07 +0200

    Night work

--- COS216/PA3/signup.php
+++ COS216/PA3/signup.php
@@ -2,11 +2,16 @@
 <!DOCTYPE html>
 <head lang="en">
     <title>CarTographer</title>
-    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/signup.css">
     <meta charset="utf-8">
 </head>
 <body>
-    <form id="form" action="./validate-signup.php" method="post">
+    <?php
+        include "header.php";
+    ?>    
+    <div id="formDiv">
+
+        <form id="form" action="./validate-signup.php" method="post">
             Name<br><input type="text" name="name" id="name"><br>
             Surname<br><input type="text" name="surname" id="surname"><br>
             Email<br><input type="text" name="email"id="email"><br>
@@ -14,31 +19,32 @@
             Password<br><input type="text" name="password" id="password" value=""><br>
             <div id="passErr"></div>
             <input type="button" value="Register" id="subBut">
-    </form>
-    <script>
-        function checkEmail(){
-            var emailBox = document.getElementById("email");
-            var emailStr = emailBox.value;
-            //console.log(emailBox);
-            //console.log(emailStr);
-            if (emailStr.search("@") === -1){
-                document.getElementById("emailErr").innerHTML = "Invalid Email";
-                return false;
+        </form>
+    </div>
+        <script>
+            function checkEmail(){
+                var emailBox = document.getElementById("email");
+                var emailStr = emailBox.value;
+                //console.log(emailBox);
+                //console.log(emailStr);
+                if (emailStr.search("@") === -1){
+                    document.getElementById("emailErr").innerHTML = "Invalid Email";
+                    return false;
+                }
+                //alert("true");
+                document.getElementById("emailErr").innerHTML = "";
+                return true;
             }
-            //alert("true");
-            document.getElementById("emailErr").innerHTML = "";
-            return true;
-        }
-        function checkPassword(){
-            var passBox = document.getElementById("password");
-            var passStr = passBox.value;
-            if (passStr.length > 8) {
-                if (passStr.search("[a-b]") === -1 ) {
-                    document.getElementById("passErr").innerHTML = "Password must contain lower case";
-                    return false;
-                } else if (passStr.search("[A-B]") === -1 ) {
-                    document.getElementById("passErr").innerHTML = "Password must contain upper case";
-                    return false;
+            function checkPassword(){
+                var passBox = document.getElementById("password");
+                var passStr = passBox.value;
+                if (passStr.length > 8) {
+                    if (passStr.search("[a-b]") === -1 ) {
+                        document.getElementById("passErr").innerHTML = "Password must contain lower case";
+                        return false;
+                    } else if (passStr.search("[A-B]") === -1 ) {
+                        document.getElementById("passErr").innerHTML = "Password must contain upper case";
+                        return false;
                 } else if (passStr.search("[0-9]")  === -1){
                     document.getElementById("passErr").innerHTML = "Password must contain number";
                     return false;
--- COS216/PA3/validate-signup.php
+++ COS216/PA3/validate-signup.php
@@ -15,9 +15,26 @@
         echo "Error: " . "<br>" . $conn->error;
 
     echo "Connected successfully";
-    $query = "INSERT INTO Users (Name, Surname, Email, Password, APIKey) VALUES ('" . $_POST["name"] . "', '" . $_POST["surname"] . "', '" . $_POST["email"] . "', '" . $_POST["password"] . "', '1234567890')";
-    if($conn->query($query) === true)
-        echo "New record created successfully";
-    else
-        echo "Error: " . $query . "<br>" . $conn->error;
+    $userPass = $_POST["password"];
+    $salt = $_POST["name"] . $_POST["surname"];
+    $userPass = $userPass . $salt;
+    $userPass = md5($userPass, false);
+    $apiKey = "";
+    for ($i=0; $i < 10; $i++) { 
+        $apiKey = $apiKey . chr(rand(0,62) + 65);
+    }
+    $query = "INSERT INTO Users (Name, Surname, Email, Password, APIKey) VALUES ('" . $_POST["name"] . "', '" . $_POST["surname"] . "', '" . $_POST["email"] . "', '" . $userPass . "', '" . $apiKey . "')";
+    try {
+        if($conn->query($query) === true)
+            echo "New record created successfully";
+        else
+            echo "Error: " . $query . "<br>" . $conn->error;
+        
+    } catch (\Throwable $th) {
+        //echo 'Email already in use';
+        //header("Location: ./signup.php");
+        //exit;
+    }
+    header("Location: ./index.php");
+    exit;
 ?>
