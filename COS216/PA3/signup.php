876f1c14a7abb264ab061ec9c14903c4f6cac055
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-04-16 11:59:52 +0200

    Update signup.php

---
+++ COS216/PA3/signup.php
@@ -0,0 +1,15 @@
+<!--Michael Horwitz u22512323-->
+<!DOCTYPE html>
+<head lang="en">
+    <title>CarTographer</title>
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        include "header.php";
+    ?>
+    <div id="form">
+        
+    </div>
+</body>
